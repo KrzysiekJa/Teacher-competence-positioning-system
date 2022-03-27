2f5ba7f77adb19a01a9b724c9738b05baf579031
KrzysiekJa <kjmj77@gmail.com>
2022-03-27 23:25:14 +0200

    Django app started working. Postgre integrated.

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-btw2(8f&#8xp@%d@a+v-wsr5-%)_xop3$!^*+wurb73o@6)qqr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
