7ae5566fde06e9ddb03bae2f75461d244ba0ddf5
KrzysiekJa <kjmj77@gmail.com>
2022-04-10 17:57:38 +0200

    Added models views in admin site (in admin.py)

--- main_app/positioning_app/admin.py
+++ main_app/positioning_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.Tutor import Tutor
+from .models.Assessment import Assessment
+from .models.Institution import Institution
 
-# Register your models here.
+
+admin.site.register(Tutor)
+admin.site.register(Assessment)
+admin.site.register(Institution)
