6975452790f04ea9cdb72852bbd822cbcb225534
KrzysiekJa <kjmj77@gmail.com>
2022-04-14 22:46:09 +0200

    Added in commit:
    1. accounts app with made views and serializers.
    2. modified urls.py in main_app.
    3. made changes in Dockerfile and requirements.txt.

---
+++ main_app/accounts/views.py
@@ -0,0 +1,44 @@
+#from django.shortcuts import render
+from .serializers import UserSerializer , AdminSerializer
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+
+
+
+class UserRegisterView(APIView):
+    
+    def get(self, request, format=None):
+        snippet = User.objects.filter(is_superuser=False)
+        serializer = UsersSerializer(snippet, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(request)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+class AdminRegisterView(APIView):
+    
+    def get(self, request, format=None):
+        snippet = User.objects.filter(is_superuser=True)
+        serializer = AdminsSerializer(snippet, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AdminSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(request)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- main_app/main_app/urls.py
+++ main_app/main_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin_logging'),
+    path('accounts/', include('accounts.urls')),
     path('', include('positioning_app.urls')),
 ]
