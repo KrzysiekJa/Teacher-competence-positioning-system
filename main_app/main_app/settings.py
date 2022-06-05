b2bdff2500b2b05d0e37ad1c286272121e2a3874
KrzysiekJa <kjmj77@gmail.com>
2022-06-05 14:22:14 +0200

    Changed authentication leven in settings.py

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
-        #'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        #'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
